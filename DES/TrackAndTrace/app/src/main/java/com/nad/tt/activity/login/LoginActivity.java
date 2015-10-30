f6d47bd111b7d245c07b07c1b5f8fa028a2ffbb8
mhernandez <mau.yeri@gmail.com>
2015-10-30 08:25:11 -0600

    Eliminacion de proyecto anterior.

--- DES/TrackAndTrace/app/src/main/java/com/nad/tt/activity/login/LoginActivity.java
+++ DES/TrackAndTrace/app/src/main/java/com/nad/tt/activity/login/LoginActivity.java
@@ -2,17 +2,31 @@ package com.nad.tt.activity.login;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.nad.tt.comun.enumeration.ElementDTO;
+import com.nad.tt.util.Constants;
 import com.nad.tt.util.Util;
 
 public class LoginActivity extends Activity {
+
+    private     EditText txtUser;
+    private EditText txtPassword;
+    private TextView lblErrorUser;
+    private TextView lblErrorPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
     }
 
     @Override
@@ -30,5 +44,42 @@ public class LoginActivity extends Activity {
 
         // Call show menu from java class util
         return Util.showMenu(id, this);
+    }
+
+    public void init(){
+        txtUser = (EditText)(findViewById(R.id.txt_user));
+        txtPassword = (EditText)(findViewById(R.id.txt_password));
+        lblErrorUser = (TextView)findViewById(R.id.lblErrorEmail);
+        lblErrorPassword = (TextView)findViewById(R.id.lblErroPassword);
+
+        ElementDTO elementEmail = new ElementDTO(txtUser, true, Constants.REGEXP_EMAIL, "Email", lblErrorUser, Constants.ERROR_CODE_OK);
+        ElementDTO elementPass = new ElementDTO(txtPassword, true, Constants.REGEXP_PASS, "Password", lblErrorPassword, Constants.ERROR_CODE_OK);
+
+        txtUser.addTextChangedListener(watcher(elementEmail));
+        txtPassword.addTextChangedListener(watcher(elementPass));
+        Log.d(Constants.LOG_NAD, "INIT");
+    }
+
+    private TextWatcher watcher(final ElementDTO dto) {
+        Log.d(Constants.LOG_NAD, "WATCHER");
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(Constants.LOG_NAD, "OnTextChanged");
+                Util.isValidComponent(dto);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        return textWatcher;
     }
 }
